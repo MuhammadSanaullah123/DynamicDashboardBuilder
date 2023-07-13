d161ae39f426bb2b2fd0e6b671666076e0de34b1
MuhammadSanaullah123 <muhammed14336@gmail.com>
2023-07-13 17:14:55 +0500

    Navbar responsice

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,48 @@ const App = () => {
   return (
     <>
       <Navbar />
-      <div id="parentDiv">
-        {widgets
-          .sort((a, b) => parseInt(a.priority) - parseInt(b.priority))
-          .map((widget, index) => (
-            <Widget widget={widget} key={index} index={index} />
-          ))}
+      <div id="fixedDiv">
+        <h3>Welcome Muhammad!</h3>
+        <div id="fourDiv">
+          <div className="twoDiv">
+            <div className="childDiv">
+              <h6>Repairs</h6>
+              <div className="hr"></div>
+
+              <p>$7546</p>
+              <p>+25%</p>
+            </div>
+            <div className="childDiv">
+              <h6>Network Unlocks</h6>
+              <div className="hr"></div>
+
+              <p>$7546</p>
+              <p>+25%</p>
+            </div>
+          </div>
+          <div className="twoDiv">
+            <div className="childDiv">
+              <h6>Accessories & parts</h6>
+              <div className="hr"></div>
+              <p>$7546</p>
+              <p>+25%</p>
+            </div>
+            <div className="childDiv">
+              <h6>Trade In</h6>
+              <div className="hr"></div>
+
+              <p>$7546</p>
+              <p>+25%</p>
+            </div>
+          </div>
+        </div>
+        <div id="parentDiv">
+          {widgets
+            .sort((a, b) => parseInt(a.priority) - parseInt(b.priority))
+            .map((widget, index) => (
+              <Widget widget={widget} key={index} index={index} />
+            ))}
+        </div>
       </div>
     </>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,38 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import react from "../assets/react.svg";
 import profilepic from "../assets/Muhammad1.jpg";
 
 const Navbar = () => {
+  const [toggle, setToggle] = useState(false);
+
   const handleScroll = () => {
     window.scrollTo(0, 0);
   };
   const handleAlert = (e) => {
     alert(`${e} is not yet developed yet!`);
   };
+  /* const handleToggle = () => {
+    
+  }; */
+  useEffect(() => {
+    if (toggle === true) {
+      const element = document.getElementById("mobile");
+      const rect = element.getBoundingClientRect();
+      const x = rect.left + window.scrollX;
+      const y = rect.top + window.scrollY;
+      let mobile = document.getElementById("ul-mobile");
+      mobile.style.position = "absolute";
+      mobile.style.left = "0px";
+      mobile.style.left = `${x - 100}px`;
+      mobile.style.top = `${y + 30}px`;
+    }
+  }, [toggle]);
+
   return (
     <div id="navbar">
       <img onClick={handleScroll} src={react} alt="" />
       <h3 onClick={handleScroll}>Dynamic Dashboard</h3>
-      <ul>
+      <ul id={`${toggle ? "ul-mobile" : ""}`}>
         <a href="#!">Inventory</a>
         <a href="#!">Customers</a>
         <a href="#!">Expense</a>
@@ -22,7 +41,8 @@ const Navbar = () => {
       </ul>
       <span>
         <i
-          onClick={(e) => handleAlert("Category Section")}
+          onClick={() => setToggle(!toggle)}
+          id="mobile"
           class="fa-solid fa-ellipsis"
         ></i>
         <i
