bc2ffa566d7f3d7c44933acd6570bf69c097e4d6
MuhammadSanaullah123 <muhammed14336@gmail.com>
2023-07-13 15:22:21 +0500

    Navbar complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
 import Widget from "./components/Widget";
+import Navbar from "./components/Navbar";
 import { widgets } from "./constants";
 import "./App.css";
 
 const App = () => {
   return (
-    <div id="parentDiv">
-      {widgets
-        .sort((a, b) => parseInt(a.priority) - parseInt(b.priority))
-        .map((widget, index) => (
-          <Widget widget={widget} key={index} index={index} />
-        ))}
-    </div>
+    <>
+      <Navbar />
+      <div id="parentDiv">
+        {widgets
+          .sort((a, b) => parseInt(a.priority) - parseInt(b.priority))
+          .map((widget, index) => (
+            <Widget widget={widget} key={index} index={index} />
+          ))}
+      </div>
+    </>
   );
 };
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,46 @@
 import React from "react";
+import react from "../assets/react.svg";
+import profilepic from "../assets/Muhammad1.jpg";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const handleScroll = () => {
+    window.scrollTo(0, 0);
+  };
+  const handleAlert = (e) => {
+    alert(`${e} is not yet developed yet!`);
+  };
+  return (
+    <div id="navbar">
+      <img onClick={handleScroll} src={react} alt="" />
+      <h3 onClick={handleScroll}>Dynamic Dashboard</h3>
+      <ul>
+        <a href="#!">Inventory</a>
+        <a href="#!">Customers</a>
+        <a href="#!">Expense</a>
+        <a href="#!">Review</a>
+        <a href="#!">Traffic</a>
+      </ul>
+      <span>
+        <i
+          onClick={(e) => handleAlert("Category Section")}
+          class="fa-solid fa-ellipsis"
+        ></i>
+        <i
+          onClick={(e) => handleAlert("Help Section")}
+          class="fa-solid fa-question"
+        ></i>
+        <i
+          onClick={(e) => handleAlert("Notification Section")}
+          class="fa-solid fa-bell"
+        ></i>
+        <img
+          onClick={(e) => handleAlert("Profile Section")}
+          src={profilepic}
+          alt=""
+        />
+      </span>
+    </div>
+  );
 };
 
 export default Navbar;
